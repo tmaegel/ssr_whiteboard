4df85e8ef7d109cef27d26a9741ca2aabcfad109
Toni Mägel <tmaegel@posteo.de>
2020-11-06 16:32:36 +0100

    updated db schema

--- whiteboard/schema.sql
+++ whiteboard/schema.sql
@@ -1,17 +1,48 @@
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS table_users;
+DROP TABLE IF EXISTS table_tags;
+DROP TABLE IF EXISTS table_equipment;
+DROP TABLE IF EXISTS table_movements;
+DROP TABLE IF EXISTS table_workout;
+DROP TABLE IF EXISTS table_workout_score;
+DROP TABLE IF EXISTS table_workout_tags;
 
-CREATE TABLE user (
+CREATE TABLE table_users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  name TEXT,
+  password TEXT
 );
-
-CREATE TABLE post (
+CREATE TABLE table_tags (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
+  userId INTEGER,
+  tag TEXT
+);
+CREATE TABLE table_equipment (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  equipment TEXT
+);
+CREATE TABLE table_movements (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movement TEXT,
+  equipmentIds TEXT
+);
+CREATE TABLE table_workout (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId INTEGER,
+  name TEXT,
+  description TEXT,
+  datetime INTEGER
+);
+CREATE TABLE table_workout_score (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId INTEGER,
+  workoutId INTEGER,
+  score TEXT,
+  rx BOOL,
+  datetime INTEGER,
+  note TEXT
+);
+CREATE TABLE table_workout_tags (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  workoutId INTEGER,
+  tagId INTEGER
 );
