3df178bf606e0248f14bd217e74022d371374d3e
Toni Mägel <tmaegel@posteo.de>
2020-11-18 16:34:08 +0100

    added project configuration

---
+++ config.py
@@ -0,0 +1,4 @@
+DEBUG = False
+SECRET_KEY = 'dev'  # Be shure to overload it by instance/config.py
+DATABASE = 'instance/whiteboard.sqlite'
+BCRYPT_LOG_ROUNDS = 12  # Configuration for the Flask-Bcrypt extension
--- whiteboard/__init__.py
+++ whiteboard/__init__.py
@@ -6,12 +6,10 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'whiteboard.sqlite'),
-    )
+    app.config.from_object('config')  # Load config.py in project directory
 
     if test_config is None:
+        # overload config with instance/config.py
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
