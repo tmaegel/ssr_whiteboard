3cf1ff9765d364565a4cd5857ee0a3d60b660d7d
Toni Mägel <tmaegel@posteo.de>
2021-01-13 11:33:49 +0100

    hide sidebar when click on document

--- whiteboard/static/js/sidebar.js
+++ whiteboard/static/js/sidebar.js
@@ -1,8 +1,14 @@
-function toggleSidebar() {
+document.addEventListener('click', function() {
+  let sidebar = document.getElementById('sidebar');
+  sidebar.style.display = 'none';
+});
+
+function toggleSidebar(event) {
   let sidebar = document.getElementById('sidebar');
   if(sidebar.style.display == 'block') {
     sidebar.style.display = 'none';
   } else {
     sidebar.style.display = 'block';
   }
+  event.stopPropagation();
 }
