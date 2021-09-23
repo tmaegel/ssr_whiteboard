a6fd035bf74bc0ea1fc7f2c105e0d3eb94c46eca
Toni Mägel <tmaegel@posteo.de>
2021-09-23 14:00:15 +0200

    Improve user model validation

--- whiteboard/models/user.py
+++ whiteboard/models/user.py
@@ -2,9 +2,7 @@
 # It will become the default in Python 3.10.
 from __future__ import annotations
 
-import sqlite3
 from typing import Any, Union
-
 from whiteboard.db import get_db
 from whiteboard.exceptions import (
     UserInvalidIdError,
@@ -12,6 +10,72 @@ from whiteboard.exceptions import (
     UserInvalidPasswordError,
     UserNotFoundError,
 )
+
+import json
+import logging
+import sqlite3
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create console handler and set level to debug
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+# Create formatter
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Add formatter to logger
+handler.setFormatter(formatter)
+# add logger
+logger.addHandler(handler)
+
+
+def validate(attr=()):
+    """
+    Decorator to validate a user object.
+
+    :param attr: Attributes of the workout object to validate.
+                 Possible values: id, name, password_hash
+    """
+
+    def _decorator(func):
+        def _wrapper(*args, **kwargs):
+            logger.debug('Call function %r with attributes %r.' % (func, attr))
+            logger.debug('Validate object %s' % args[0])
+            if 'name' in attr:
+                _validate_user_name(args[0].name)
+            if 'password_hash' in attr:
+                _validate_user_password_hash(args[0].password_hash)
+            if 'id' in attr:
+                _validate_user_id(args[0].user_id)
+            return func(*args, **kwargs)
+        return _wrapper
+
+    def _validate_user_id(user_id: Any) -> None:
+        """Validate the user id."""
+        logger.debug('Validate user id.')
+        if user_id is None or isinstance(user_id, bool):
+            raise UserInvalidIdError()
+        try:
+            user_id = int(user_id)
+        except (ValueError, TypeError):
+            raise UserInvalidIdError()
+        if user_id < 0:
+            raise UserInvalidIdError()
+
+    def _validate_user_name(name: Any) -> Any:
+        """Validate the user name."""
+        logger.debug('Validate user name.')
+        if name is None or not isinstance(name, str):
+            raise UserInvalidNameError()
+
+    def _validate_user_password_hash(password_hash: Any) -> None:
+        """Validate the user password_hash."""
+        if password_hash is None or not isinstance(password_hash, str):
+            raise UserInvalidPasswordError()
+
+    return _decorator
 
 
 class User():
@@ -21,14 +85,15 @@ class User():
         self.name = name
         self.password_hash = password_hash
 
-        self._db = get_db()
-
     def __str__(self):
         return f'User ( user_id={self.user_id}, name={self.name} )'
 
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
     @property
     def db(self):
-        return self._db
+        return get_db()
 
     @staticmethod
     def _query_to_object(query: sqlite3.Row) -> Union[User, None]:
@@ -41,32 +106,6 @@ class User():
             query['name'],
             query['password'],  # password=password_hash
         )
-
-    @staticmethod
-    def _validate_id(user_id: Any) -> None:
-        """Validate the user id."""
-        # @todo: Check if user exists by reqeuesting it.
-        if user_id is None or isinstance(user_id, bool):
-            raise UserInvalidIdError()
-        try:
-            user_id = int(user_id)
-        except (ValueError, TypeError):
-            raise UserInvalidIdError()
-        if user_id < 0:
-            raise UserInvalidIdError()
-
-    @staticmethod
-    def _validate_name(name: Any) -> None:
-        """Validate the user name."""
-        # @todo: Allow only [a-zA-Z0-9]
-        if name is None or not isinstance(name, str):
-            raise UserInvalidNameError()
-
-    @staticmethod
-    def _validate_password_hash(password_hash: Any) -> None:
-        """Validate the user password_hash."""
-        if password_hash is None or not isinstance(password_hash, str):
-            raise UserInvalidPasswordError()
 
     @staticmethod
     def exist_user_id(user_id: int) -> bool:
@@ -87,6 +126,7 @@ class User():
         else:
             return True
 
+    @validate(attr=('id'))
     def get(self) -> User:
         """
         Get user from db by id.
@@ -94,7 +134,6 @@ class User():
         :return: User object
         :rtype: User
         """
-        User._validate_id(self.user_id)
         result = self.db.execute(
             'SELECT id, name, password FROM table_users'
             ' WHERE id = ?', (self.user_id,)
@@ -106,6 +145,7 @@ class User():
 
         return user
 
+    @validate(attr=('name'))
     def get_by_name(self) -> User:
         """
         Get user from db by name.
@@ -113,7 +153,6 @@ class User():
         :return: User object
         :rtype: User
         """
-        User._validate_name(self.name)
         result = self.db.execute(
             'SELECT id, name, password FROM table_users'
             ' WHERE name = ?', (self.name,)
@@ -125,6 +164,7 @@ class User():
 
         return user
 
+    @validate(attr=('name', 'password_hash'))
     def add(self) -> bool:
         """
         Add new user to db.
@@ -134,8 +174,6 @@ class User():
         """
         # @todo: bcrypt + hash here?
         # gen_password_hash() + check_password() function?
-        User._validate_name(self.name)
-        User._validate_password_hash(self.password_hash)
         self.db.execute(
             'INSERT INTO table_users'
             ' (name, password)'
@@ -145,6 +183,7 @@ class User():
 
         return True
 
+    @validate(attr=('id', 'name', 'password_hash'))
     def update(self) -> bool:
         """
         Update user in db by id.
@@ -154,10 +193,6 @@ class User():
         """
         # @todo: bcrypt + hash here?
         # gen_password_hash() + check_password() function?
-        User._validate_id(self.user_id)
-        User._validate_name(self.name)
-        User._validate_password_hash(self.password_hash)
-
         if not User.exist_user_id(self.user_id):
             raise UserNotFoundError(identifier=self.user_id)
 
@@ -171,6 +206,7 @@ class User():
 
         return True
 
+    @validate(attr=('id'))
     def Remove(self) -> bool:
         """Remove user from db by id."""
         pass
